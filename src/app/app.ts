2f36fc93a82c071304f0e08fceb407dc1bb87f1f
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-07-17 15:06:32 -0400

    Add home page componenet and add logo.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {Home} from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [Home],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.css'],
 })
 export class App {
-  protected readonly title = signal('my-app');
+  title = 'homes';
 }
