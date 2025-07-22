5e1887ef7eb506eb64a6d41ad8b5aaf024d225cf
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-07-21 21:53:44 -0400

    Add the pokemon component and start working on the styling of the card.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,12 +1,18 @@
 import {Routes} from '@angular/router';
 import {Home} from './home/home';
 import {Details} from './details/details';
+import { Pokemon } from './pokemon/pokemon';
 
 const routeConfig: Routes = [
   {
     path: '',
     component: Home,
     title: 'Home page',
+  },
+  {
+    path: 'pokemon',
+    component: Pokemon,
+    title: 'Pokemon',
   },
   {
     path: 'details/:id',
